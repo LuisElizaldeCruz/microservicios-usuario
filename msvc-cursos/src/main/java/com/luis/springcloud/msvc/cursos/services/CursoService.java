aa21d1ff9064bf7fa59882dbcd3a3f5ebafc556e
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2025-03-04 19:48:34 -0600

    creación de la estructura del microservicio cursos

---
+++ msvc-cursos/src/main/java/com/luis/springcloud/msvc/cursos/services/CursoService.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.msvc.cursos.services;
+
+import com.luis.springcloud.msvc.cursos.entity.Curso;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CursoService {
+    List<Curso> listar();
+    Optional<Curso> porId(Long id);
+    Curso guardar (Curso curso);
+    void eliminar (Long id);
+}
