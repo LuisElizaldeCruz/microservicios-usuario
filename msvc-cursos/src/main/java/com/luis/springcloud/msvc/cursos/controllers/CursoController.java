a1093fd6303bf5bb7779bce659e07cbd2caae6c7
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2025-03-20 19:36:54 -0600

    configuración del controlador del microservicio curso

--- msvc-cursos/src/main/java/com/luis/springcloud/msvc/cursos/controllers/CursoController.java
+++ msvc-cursos/src/main/java/com/luis/springcloud/msvc/cursos/controllers/CursoController.java
@@ -3,10 +3,9 @@ package com.luis.springcloud.msvc.cursos.controllers;
 import com.luis.springcloud.msvc.cursos.entity.Curso;
 import com.luis.springcloud.msvc.cursos.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,7 +28,34 @@ public class CursoController {
             return ResponseEntity.ok(o.get());
         }
 
-        return ResponseEntity.notFound().build(); //devuelve una respuesta 404 si el curso no existe
+        return ResponseEntity.notFound().build(); //devuelve una respuesta 404 si el recurso no existe
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> crear(@RequestBody Curso curso){
+        Curso cursoDb = service.guardar(curso);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cursoDb);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@PathVariable Long id, @RequestBody Curso curso){
+        Optional<Curso> o = service.porId(id);
+        if(o.isPresent()){
+            Curso cursoDb = o.get();
+            cursoDb.setNombre(curso.getNombre());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(cursoDb));
+        }
+        return ResponseEntity.notFound().build(); //devuelve una respuesta 404 si el recurso no existe
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Curso> o = service.porId(id);
+        if(o.isPresent()){
+            service.eliminar(id);
+            return ResponseEntity.noContent().build(); //devuelve una respuesta 204
+        }
+        return ResponseEntity.notFound().build(); //devuelve una respuesta 404 si el recurso no existe
     }
 
 }
